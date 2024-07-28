e489350bbfb285729065dec0187e416669d19c7f
Surventurer <abhinavrajpati3344@gmail.com>
2024-07-28 11:13:46 +0530

    empty bug fixed

--- js/email.js
+++ js/email.js
@@ -1,22 +1,29 @@
 function sendMail(){
     let parms = {
-        name : document.getElementById('name').value,
-        email : document.getElementById("email").value,
-        phoneno : document.getElementById("phoneno").value,
-        message : document.getElementById("message").value
+        name: document.getElementById('name').value,
+        email: document.getElementById("email").value,
+        phoneno: document.getElementById("phoneno").value,
+        message: document.getElementById("message").value
     };
-    console.log(parms);
-    emailjs.send("service_cv2fx1f","template_7zby97m",parms).then(alert("Email Sent!!"));
     
+    if (parms.name && parms.email && parms.phoneno && parms.message) {
+        emailjs.send("service_cv2fx1f","template_7zby97m",parms).then(() => alert("Email Sent!!"));
+    } else {
+        alert("All fields are required.");
+    }
 }
+
 function sendMail1(){
     let parms1 = {
-        name : document.getElementById('name1').value,
-        email : document.getElementById("email1").value,
-        phoneno : document.getElementById("phoneno1").value,
-        message : document.getElementById("message1").value
+        name: document.getElementById('name1').value,
+        email: document.getElementById("email1").value,
+        phoneno: document.getElementById("phoneno1").value,
+        message: document.getElementById("message1").value
     };
-    console.log(parms1);
-    emailjs.send("service_cv2fx1f","template_7zby97m",parms1).then(alert("Email Sent!!"));
     
+    if (parms1.name && parms1.email && parms1.phoneno && parms1.message) {
+        emailjs.send("service_cv2fx1f","template_7zby97m",parms1).then(() => alert("Email Sent!!"));
+    } else {
+        alert("All fields are required.");
+    }
 }
