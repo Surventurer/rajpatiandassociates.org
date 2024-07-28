0ecec698622088d849c6dbb37678421e996a1895
Surventurer <abhinavrajpati3344@gmail.com>
2024-07-28 11:33:19 +0530

    fixed bug 2

--- js/email.js
+++ js/email.js
@@ -1,4 +1,9 @@
-function sendMail(){
+function isValidEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailPattern.test(email);
+}
+
+function sendMail() {
     let parms = {
         name: document.getElementById('name').value,
         email: document.getElementById("email").value,
@@ -6,14 +11,16 @@ function sendMail(){
         message: document.getElementById("message").value
     };
     
-    if (parms.name && parms.email && parms.phoneno && parms.message) {
-        emailjs.send("service_cv2fx1f","template_7zby97m",parms).then(alert("Email Sent!!"));
-    } else {
+    if (!parms.name || !parms.email || !parms.phoneno || !parms.message) {
         alert("All fields are required.");
+    } else if (!isValidEmail(parms.email)) {
+        alert("Please enter a valid email address.");
+    } else {
+        emailjs.send("service_cv2fx1f", "template_7zby97m", parms).then(alert("Email Sent!!"));
     }
 }
 
-function sendMail1(){
+function sendMail1() {
     let parms1 = {
         name: document.getElementById('name1').value,
         email: document.getElementById("email1").value,
@@ -21,9 +28,11 @@ function sendMail1(){
         message: document.getElementById("message1").value
     };
     
-    if (parms1.name && parms1.email && parms1.phoneno && parms1.message) {
-        emailjs.send("service_cv2fx1f","template_7zby97m",parms1).then(alert("Email Sent!!"));
-    } else {
+    if (!parms1.name || !parms1.email || !parms1.phoneno || !parms1.message) {
         alert("All fields are required.");
+    } else if (!isValidEmail(parms1.email)) {
+        alert("Please enter a valid email address.");
+    } else {
+        emailjs.send("service_cv2fx1f", "template_7zby97m", parms1).then(alert("Email Sent!!"));
     }
 }
